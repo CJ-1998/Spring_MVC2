502954e8bbad9284dee191b3ddf78bbaf01738c6
CJ <choijun0627@konkuk.ac.kr>
2023-07-28 18:55:58 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 1
    타임리프 기본 객체들 공부
    
    타임리프에서 기본으로 제공하는 객체들 공부하기 위한 컨트롤러와 스프링 빈
    BasicController 클래스에 추가
    기본 객체들 사용 보기 위한 뷰인 basic-object.html 생성

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,13 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -48,6 +50,18 @@ public class BasicController {
         public User(String username, int age) {
             this.username = username;
             this.age = age;
+        }
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
         }
     }
 }
