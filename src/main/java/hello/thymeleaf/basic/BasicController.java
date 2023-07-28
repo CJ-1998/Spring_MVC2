e901f6b56e5e7ffef505579e357c12a8fb684f79
CJ <choijun0627@konkuk.ac.kr>
2023-07-29 00:01:12 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 1
    타임리프 연산 공부
    
    타임리프 연산 공부하기 위한 컨트롤러와 뷰인 operation.html 추가

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -84,4 +84,11 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/literal";
     }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
 }
