c465b09bf89a1d812925969f0afc5e0c06fe674f
CJ <choijun0627@konkuk.ac.kr>
2023-07-29 18:38:05 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 1
    타임리프 태그 속성 공부
    
    타임리프의 th: 하면 어떻게 되는지 보기 위한 controller 추가 및  view인attribute.html 생성
    속성 앞 뒤에 추가하는 th:attrappend, th:attrprepend나 checked 처리
    도와주는 th:checked 배움

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -91,4 +91,9 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/operation";
     }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
 }
