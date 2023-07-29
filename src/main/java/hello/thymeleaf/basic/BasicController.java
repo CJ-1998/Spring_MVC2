cec5041a9e0d895ef8827d359371dc7c0afa0d80
CJ <choijun0627@konkuk.ac.kr>
2023-07-29 18:54:38 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 1
    타임리프 반복 공부
    
    반복 확인 위한 컨트롤러 추가와 뷰인 each.html 생성
    th:each로 for:each문처럼 반복할 수 있다.
    th:each의 첫번째 변수명 + Stat 이름으로 반복문의 상태에 대한 값들 얻을 수 있다.

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -96,4 +96,19 @@ public class BasicController {
     public String attribute() {
         return "basic/attribute";
     }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
+
+
 }
