b6b43d49788dc74f93a15fcebedbaac608a29448
CJ <choijun0627@konkuk.ac.kr>
2023-07-29 22:49:14 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 1
    자바스크립트 인라인 공부
    
    자바스크립트 인라인 공부 위한 컨트롤러 추가 및 뷰인 javascript.html 추가
    자바스크립트에서 텍스트 랜더링, 내추럴 템플릿, 객체, 인라인 each 배움

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -128,4 +128,11 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
 }
