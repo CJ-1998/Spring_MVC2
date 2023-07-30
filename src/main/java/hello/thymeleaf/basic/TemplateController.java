1d39f25e521b8bfda7dfd2af4ffa85c3ae0aac7a
CJ <choijun0627@konkuk.ac.kr>
2023-07-31 01:29:48 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 1
    타임리프 템플릿 레이아웃 확장 공부
    
    html 자체를 레이아웃으로 두고 내가 원하는 부분만 교체해서 쓰는 것
    템플릿 레이아웃 확장 위한 컨트롤러 추가
    html 전체가 레이이앗인 layoutFile.html 생성
    내 페이지로 레이아웃에서 조금만 바꿔서 쓰려는 layoutExtendMain.html 생성

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -17,4 +17,8 @@ public class TemplateController {
         return "template/layout/layoutMain";
     }
 
+    @GetMapping("/layoutExtend")
+    public String layoutExtends() {
+        return "template/layoutExtend/layoutExtendMain";
+    }
 }
